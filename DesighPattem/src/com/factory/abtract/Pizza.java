7d814ff160906ea07855c986bc62c07d8ffe371b
luongbangnguyen <luongbangvh@gmail.com>
2015-03-05 00:03:33 +0700

    Adding model class and interfaces

---
+++ DesighPattem/src/com/factory/abtract/Pizza.java
@@ -0,0 +1,75 @@
+package com.factory.abtract;
+
+import com.factory.abtract.ingredient.Chesse;
+import com.factory.abtract.ingredient.Clams;
+import com.factory.abtract.ingredient.Dough;
+import com.factory.abtract.ingredient.Pepperoni;
+import com.factory.abtract.ingredient.Sauce;
+import com.factory.abtract.ingredient.Veggies;
+
+public class Pizza {
+	private String name;
+	private Dough dough;
+	private Sauce sauce;
+	private Veggies veggies[];
+	private Chesse cheese;
+	private Pepperoni pepperoni;
+	private Clams clam;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Dough getDough() {
+		return dough;
+	}
+
+	public void setDough(Dough dough) {
+		this.dough = dough;
+	}
+
+	public Sauce getSauce() {
+		return sauce;
+	}
+
+	public void setSauce(Sauce sauce) {
+		this.sauce = sauce;
+	}
+
+	public Veggies[] getVeggies() {
+		return veggies;
+	}
+
+	public void setVeggies(Veggies[] veggies) {
+		this.veggies = veggies;
+	}
+
+	public Chesse getCheese() {
+		return cheese;
+	}
+
+	public void setCheese(Chesse cheese) {
+		this.cheese = cheese;
+	}
+
+	public Pepperoni getPepperoni() {
+		return pepperoni;
+	}
+
+	public void setPepperoni(Pepperoni pepperoni) {
+		this.pepperoni = pepperoni;
+	}
+
+	public Clams getClam() {
+		return clam;
+	}
+
+	public void setClam(Clams clam) {
+		this.clam = clam;
+	}
+
+}
---
+++ DesighPattem/src/com/factory/abtract/ingredient/Dough.java
@@ -0,0 +1,5 @@
+package com.factory.abtract.ingredient;
+
+public interface Dough {
+
+}
